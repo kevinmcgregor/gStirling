2fa64b0beb970c8bb1ba106b7bb40cc3f2effb99
Kevin McGregor <kevin.mcgregor@mail.mcgill.ca>
2020-05-15 17:07:16 -0400

    adding default for iter argument

--- R/sampConc.R
+++ R/sampConc.R
@@ -13,7 +13,7 @@
 #' @export
 #'
 #' @examples
-sampConc <- function(conc, n, tab, p.shape, p.scale, dsct, iter) {
+sampConc <- function(conc, n, tab, p.shape, p.scale, dsct, iter=1) {
   .Call("sampConc_", as.double(conc), as.integer(n), as.integer(tab),
         as.double(p.shape), as.double(p.scale), dsct, as.integer(iter))
 }
