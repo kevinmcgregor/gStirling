e0e955a5bc2a1a450dc907948a055fc85fc2ed76
Kevin McGregor <kevin.mcgregor@mail.mcgill.ca>
2020-05-15 17:00:21 -0400

    adding iter argument to sampConc_

--- src/sample_concentration.c
+++ src/sample_concentration.c
@@ -8,11 +8,12 @@
 #include "srng.h"
 #include "lgamma.h"
 
-SEXP sampConc_(SEXP b_in, SEXP N, SEXP T, SEXP shape, SEXP scale, SEXP dsct) {
+SEXP sampConc_(SEXP b_in, SEXP N, SEXP T, SEXP shape, SEXP scale, SEXP dsct, SEXP iter) {
   double b = REAL(b_in)[0];
   double sh = REAL(shape)[0];
   double sc = REAL(scale)[0];
   double d = REAL(dsct)[0];
+  int itr = INTEGER(iter)[0];
 
   rngp_t rng = NULL;
 
@@ -20,7 +21,7 @@ SEXP sampConc_(SEXP b_in, SEXP N, SEXP T, SEXP shape, SEXP scale, SEXP dsct) {
   scnt_int tempT = (scnt_int) INTEGER(T)[0];
 
   SEXP result = PROTECT(allocVector(REALSXP, 1));
-  REAL(result)[0] = sampleb(b, 1, sh, sc, &tempN, &tempT, d, rng, 5, 1);
+  REAL(result)[0] = sampleb(b, 1, sh, sc, &tempN, &tempT, d, rng, itr, 1);
   UNPROTECT(1);
   return result;
 }
